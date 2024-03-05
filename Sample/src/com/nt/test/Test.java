10fb96ecea9b2b20ed8226ba74424c9546094545
Dev2 <Dev2@gmail.com>
2024-03-05 23:26:39 +0530

    test method updated

--- Sample/src/com/nt/test/Test.java
+++ Sample/src/com/nt/test/Test.java
@@ -4,6 +4,7 @@ import com.nt.beans.Utility;
 
 public class Test {
 	public static void main(String[] args) {
+		System.out.println("Test.main()");
 		Utility u= new Utility();
 		System.out.println(u.createDate());
 	}
